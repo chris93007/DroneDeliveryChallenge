e790f663c62afdbd68f5dc458e670feb6bb12298
CJ <chris93007@gmail.com>
2018-10-28 23:42:09 -0400

    asycn waterfall

--- scheduler.js
+++ scheduler.js
@@ -1,14 +1,35 @@
+var async = require("async");
 var utils = require("./utils");
 
 var exports = module.exports = {};
 
 exports.droneSchedule = function (arr) {
     var promise = new Promise(function (resolve, reject) {
-        //  console.log(`Building Drone Schedule...`,arr.orderList,arr.totalOrders);
         console.log(`Building Drone Schedule...`);
-        // arr = utils.sortJSONByTwoProps(arr);
-        // console.log(arr);
-        resolve();
+        arr.orderList.sort(function (a, b) {
+            return a["distanceFromWarehouse"] - b["distanceFromWarehouse"] || a["orderTime"] - b["orderTime"];
+        });
+
+        async.waterfall([async.apply(recursivelyCheckList, arr.orderList)], function (err, results) {
+            console.log(`results`, results);
+            resolve();
+        })
+
     });
     return promise;
 }
+
+recursivelyCheckList = (sortedArray, callback) => {
+    if (sortedArray.length) {
+        console.log(sortedArray.length);
+        sortedArray.shift();
+        recursivelyCheckList(sortedArray, callback);
+
+    }
+    else {
+        console.log('done')
+        callback(null, []);
+    }
+
+}
+
--- utils.js
+++ utils.js
@@ -1,16 +1,12 @@
 var exports = module.exports = {};
 
-exports.findDistance= function(coords){
-    var dist=0;
+exports.findDistance = function (coords) {
+    var dist = 0;
     var numbers = coords.match(/\d+/g).map(Number);
-    dist=Math.sqrt(Math.pow(numbers[0],2) + Math.pow(numbers[1],2));
+    dist = Math.sqrt(Math.pow(numbers[0], 2) + Math.pow(numbers[1], 2));
     return Math.round(dist * 100) / 100;
 }
 
-exports.sortJSONByTwoProps=function(arr){
-    arr.sort(function(a, b) {
-        return a["distanceFromWarehouse"] - b["distanceFromWarehouse"] || a["orderTime"] - b["orderTime"];
-    });
-    console.log(arr)
-    return arr;
-}
+
+
+
