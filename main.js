1b2694fc717cf65f939a54c0327becf7fabf96e1
CJ <chris93007@gmail.com>
2018-11-01 23:51:45 -0400

    #new done

--- main.js
+++ main.js
@@ -11,18 +11,18 @@ var scheduler = require("./scheduler");
 */
 
 function main() {
-    // fileOps.getFileFromCLI()
-    // .then(fileOps.processFile)
-    // .then(scheduler.droneSchedule)
-    // .then(fileOps.generateOutputFile)
-    // .then(()=>{
-    //     process.exit();
-    // });
-    fileOps.processFile(".\\inputs\\input7.txt")
+    fileOps.getFileFromCLI()
+    .then(fileOps.processFile)
     .then(scheduler.droneSchedule)
     .then(fileOps.generateOutputFile)
     .then(()=>{
         process.exit();
     });
+    // fileOps.processFile(".\\inputs\\input7.txt")
+    // .then(scheduler.droneSchedule)
+    // .then(fileOps.generateOutputFile)
+    // .then(()=>{
+    //     process.exit();
+    // });
 }
 main();
